b930b5f6834f29bc4848a703cbdaacb070e7fc69
umairx97 <umair.bajwa@hotmail.com>
2019-09-26 00:32:53 +0500

    env changes

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
 
@@ -30,6 +31,24 @@ const userSchema = mongoose.Schema({
 
 })
 
-const User = mongoose.model("User", userSchema);
 
+userSchema.pre("save", function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err);
+
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            if (err) return next(err);
+
+            this.password = hash;
+            next()
+        })
+    })
+})
+
+
+const User = mongoose.model("User", userSchema);
 module.exports = User;
